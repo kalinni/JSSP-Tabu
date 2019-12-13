d7f1a46ff37e9ba84ca95d8a1c568950877ecdd0
Katja Linnemann <kalili94@web.de>
2019-12-13 16:44:25 +0100

    Added methods to get readable output

--- operation.py
+++ operation.py
@@ -1,9 +1,17 @@
 class Operation:
     """Operations form the various steps of a job for the JSSP"""
+
     def __init__(self, job, step, duration, machine):
         self.job=job
         self.step=step
         self.duration=duration
         self.machine=machine
+
     def __eq__(self, other):
         return self.job == other.job and self.step == other.step
+
+    def __str__(self):
+        return "Job %s.%s: duration %s, machine %s" % (self.job,self.step,self.duration,self.machine)
+        
+    def __repr__(self):
+    	return "<Operation job:%s step:%s, duration:%s, machine:%s>" % (self.job,self.step,self.duration,self.machine)
