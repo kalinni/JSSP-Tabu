3419d2b15cf07e143bff6fd2f66ac2bb3e2857aa
DDeckert <s7110678@mailbox.tu-dresden.de>
2019-12-17 15:15:46 +0100

    realize_plan Output verbessert
    Output auf 2 Werte reduziert:
    -schedule als 2d-Liste
    -benötigte Zeit (-1 falls die Schedule ungültig ist)

--- realize_plan.py
+++ realize_plan.py
@@ -35,8 +35,18 @@ def realize_plan (plan):
 					schedule[m].append( ( plan[m][next[m]], start, finish ) )
 					next[m] += 1
 					if s < steps -1:
-						enabled[j][s+1] = finish #der nächste Step wird zum Ende des vorherigen enabled
-	finished = True    #finished ist True wenn alle steps einsortiert wurden
+						enabled[j][s+1] = finish #der nächste Step wird enabled zum Ende des vorherigen 
+	#finished testet ob alle steps einsortiert wurden (dh ob die schedule zulässig ist)
+	finished = True    
 	for m in range(machines):
 		finished = finished and (next[m] >= steps)
-	return [finished, schedule, enabled]
+
+	#time ermittelt die benötigte Zeit falls die schedule zulässig ist
+	time =-1
+	if finished:
+		for m in range(machines):
+			time = max(time, schedule[m][-1][2])
+
+	#Ausgabe: schedule -> Liste von 3-Tupeln (Step, Startzeit, Endzeit) je Maschine
+	#		  time   ->   benötigte Zeit der Schedule (-1 falls die Schedule ungütig ist)
+	return [schedule, time]
