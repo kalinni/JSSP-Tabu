b6233da731f3ce1a46299d0aba3c7e0e2c3fdfaa
Katja Linnemann <kalili94@web.de>
2019-12-17 11:21:57 +0100

    scheduler.py added, this file contains the main function

---
+++ scheduler.py
@@ -0,0 +1,35 @@
+import sys
+from os import path
+from instanceparser import parse_instance
+from realizePlan import realizePlan
+
+def main():
+	# Decide, on which instance to test our code
+	if len(sys.argv) > 1:
+		# you may run the script with a path to an instance as the argument
+		if path.exists(sys.argv[1]):
+			instance = sys.argv[1]
+		else: 
+			raise SystemExit("There is no instance at %s" % sys.argv[1])
+	else: 
+		# default if no argument is passed
+		instance = 'instances/abz5.txt'
+
+	# Run te code we have so far
+	plan = parse_instance(instance)
+	schedule = realizePlan(plan)
+
+	# Output to check our results
+	if not schedule[0]: 
+		print("Couldn't schedule all jobs with this plan")
+	else:
+		for m in range(len(schedule[1])):
+			print("Machine %s:" % m)
+			for op in schedule[1][m]:
+				if op[0].machine != m: 
+					print("Job %s is on the wrong machine." % op[0])
+				if op[2] - op[1] != op[0].duration:
+					print("Job %s takes a wrong amount of time, it takes %s" % (op[0], op[2]-op[1]))
+				print('--- %s.%s start %s duration %s' % (op[0].job, op[0].step, op[1], op[0].duration))
+
+if __name__ == "__main__": main()
