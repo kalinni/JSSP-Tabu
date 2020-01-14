661a55405e83ea3057d266022d4747a2344ef458
DDeckert <s7110678@mailbox.tu-dresden.de>
2020-01-14 15:48:42 +0100

    Aspiration Search & Random Initial Plan
    -print_schedule now has check for overlapping steps of the same schedule
    -Aspiration Search is implemented and accepts Tabu moves if they are strictly better than any explored move (including the best regular move)
    -method random_instance constructs a random plan (always admissible due to the construction)
    -Tabu search is iterated from different random starting plans
    -scheduler.py now has an ITERATIONS parameter for the number of iterations of Tabu search

--- scheduler.py
+++ scheduler.py
@@ -3,11 +3,15 @@ from os import path
 from instanceparser import parse_instance
 from realize_plan import realize_plan
 from neighbourhood import generate_neighbour
+import random
 
 RECENCY_MEMORY = 5
 NO_IMPROVE_MAX = 20
+ITERATIONS = 5
 
-def print_schedule(schedule):
+
+def print_schedule(schedule, plan):
+	timetable = [ [ 0 for s in range(plan['steps']) ] for j in range(plan['jobs']) ]
 	if schedule[1] < 0: 
 		print("Couldn't schedule all jobs with this plan")
 	else:
@@ -19,13 +23,51 @@ def print_schedule(schedule):
 					print("Job %s is on the wrong machine." % op[0])
 				if op[2] - op[1] != op[0].duration:
 					print("Job %s takes a wrong amount of time, it takes %s" % (op[0], op[2]-op[1]))
-				print('--- %s.%s start %s duration %s' % (op[0].job, op[0].step, op[1], op[0].duration))
+				print('--- %s.%s start %s duration %s end %s' % (op[0].job, op[0].step, op[1], op[0].duration, op[2]))
+				timetable[op[0].job][op[0].step] = (op[1], op[2])
 
-def search_schedule(instance):
+	for t in timetable:
+		for i in range(len(t)):
+			if i<=len(t) - 1:
+				if t[i][1] > t[i+1][0]:
+					print("Steps from the same Job overlap: %s" % t)
 
-	# Get a plan from the chosen instance and turn it into the initial schedule
-	### Note: Will always give a valid schedule, due to how we get the plan from the instance
-	plan = parse_instance(instance) # plan is a dict
+
+
+
+def random_instance(plan):	#constructs a random schedule by repeatedly picking the next step to be sorted
+	next_step = [0 for i in range(plan['jobs'])]
+	new_plan = dict()
+
+	new_plan['machines'], new_plan['jobs'], new_plan['steps'] = plan['machines'], plan['jobs'], plan['steps']
+	for i in range(new_plan['machines']):
+		new_plan[i] = []
+
+	finished = True
+	for n in next_step:
+		finished = finished and not (n < new_plan['steps'])
+	while not finished:
+		step = False
+		options = [ i for i in range(new_plan['jobs']) if next_step[i] < new_plan['steps'] ]	#Diese Konstruktion nimmt an, dass alle Jobs gleich viele Steps haben
+		job = random.choice(options)
+		for m in range(plan['machines']):
+			i = 0
+			while i < len(plan[m]) and not step:
+				if plan[m][i].job == job and plan[m][i].step == next_step[job] and not step:
+					step = plan[m][i]
+					new_plan[m].append(step)
+					next_step[job] += 1
+					step = True
+				i += 1
+		finished = True
+		for n in next_step:
+			finished = finished and not (n < new_plan['steps'])
+	
+	return new_plan
+
+
+
+def search_schedule(plan):
 	schedule = realize_plan(plan) # schedule is a tuple, first entry is list of lists of triples, the actual schedule, second entry is the time
 
 	machines = plan['machines']
@@ -52,6 +94,21 @@ def search_schedule(instance):
 		if best_time == -1:
 			print("No valid neighbour found.")
 			break
+		
+		# Aspiration Search: If there is tabu neighbour with time better than current optimum 
+		# 						and best found swap, choose tabu swap instead
+		threshold = min(best_time, best_schedule[1])
+		for (m, i) in tabus:
+			neighbour = generate_neighbour(plan, m, i)
+			if neighbour != False:
+				sched = realize_plan(neighbour)
+				if sched[1] > 0:
+					if (sched[1] < threshold):
+						best_neighbour = sched
+						best_time = sched[1]
+						threshold = best_time
+						swap = (m, i)
+		
 		print("Swapped: %s -- Current schedule's time: %s" % (swap,best_time))
 
 		# Update the best schedule and no-improvement-counter
@@ -87,10 +144,26 @@ def main():
 		# default if no argument is passed
 		instance = 'instances/simple.txt'
 
+	# Make a plan from the chosen instance and turn it into the initial schedule
+	### Note: Will always give a valid schedule, due to how we get the plan from the instance
+	plan = parse_instance(instance) # plan is a dict
+
 	# Run the tabu search
-	best_schedule = search_schedule(instance)
+	optimum = -1
+	for i in range(ITERATIONS):
+		best_schedule = search_schedule(plan)
+		print("++++++++++++++++++++")
+		print("Best Schedule in Run: %s" % best_schedule[1])
+		if optimum < 0:
+			optimum = best_schedule[1]
+			optimum_schedule = best_schedule
+		else :
+			if best_schedule[1] < optimum:
+				optimum = best_schedule[1]
+				optimum_schedule = best_schedule
+		plan = random_instance(plan)
 
 	# Output
-	print_schedule(best_schedule)
+	print_schedule(optimum_schedule, plan)
 
 if __name__ == "__main__": main()
