9c39ec6881af683d799a9d14847155b92353d65a
DDeckert <s7110678@mailbox.tu-dresden.de>
2020-01-30 15:14:05 +0100

    Completed Serial Experiments

--- evaluator.py
+++ evaluator.py
@@ -12,21 +12,31 @@ INSTANCES_ALL = ['abz5', 'abz6', 'abz7', 'abz8', 'abz9', 'ft06', 'ft10', 'ft20',
 
 MORE_INSTANCES = ['la31', 'la32', 'la33', 'la34', 'la35', 'la36', 'la37', 'la38', 'la39', 'la40']
 
-INSTANCES = ['abz5', 'abz9', 'ft06', 'ft20', 
-	'la01', 'la05', 'la07', 'la10', 'la13', 'la18', 'la23', 'la25', 'la27', 'la28', 
-	'orb01', 'orb04', 'orb10', 'swv01', 'swv05', 'swv07', 'swv08',
-	'swv12', 'swv13', 'swv18', 'yn4']
+INSTANCES = [
+	'ft06', # 6x6
+	'la01', 'la05', # 10x5
+	'la07', 'la10', # 15x5
+	'abz5', 'la18', 'orb01', 'orb04', 'orb10', # 10x10
+	'ft20', 'la13', # 20x5
+	'la23', 'la25', # 15x10
+	'la27', 'la28', 'swv01', 'swv05', # 20x10
+	'la38', 'la39', # 15x15
+	'la31', 'la34', # 30x10
+	'abz9', 'swv07', 'swv08', # 20x15 
+	'yn4', #20x20
+	'swv12', 'swv13', 'swv18' #50x10
+]
 
 # This function is used to perform tabu search on all benchmarked instances from the lecture
 # It iterates over these instances and stores for all instances 
 # 	the best time, 
 # 	the best schedule 
 # 	the resulting times from all other iterations of tabu search on this instance
-def serial_experiments ():
+def serial_experiments (instances = INSTANCES):
 	mode = 'Experimental' # For better comparison we work with fixed starting points
 	result = dict()
 
-	for instance in INSTANCES:
+	for instance in instances:
 		print("#########################")
 		print('Now running instance: '+ instance)
 		print("#########################")
@@ -37,8 +47,8 @@ def serial_experiments ():
 	pickle.dump(result, storage)				# to a txt file
 	storage.close()
 
-def output_serial_results ():
-	file = open('results/results.txt', 'rb')
+def output_serial_results (path = 'results/results.txt'):
+	file = open(path, 'rb')
 	result = pickle.load(file)
 
 	for instance in result:
