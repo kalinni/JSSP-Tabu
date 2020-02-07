74c253ed6830074072c1815ecc8ef08da18e3451
DDeckert <s7110678@mailbox.tu-dresden.de>
2020-02-07 15:58:22 +0100

    Neue Parameter

--- searcher.py
+++ searcher.py
@@ -154,7 +154,11 @@ def search_schedule(plan):
 	print("valid: %s, invalid: %s, aspiration %s, iterations %s" % (valid,invalid,aspiration, iteration))
 	return best_schedule
 
+<<<<<<< HEAD
 def set_dynamic_parameters(plan, weights=(0.5, 2, 0.3)):
+=======
+def set_dynamic_parameters(plan, weights=(10, 2, 0.25)):
+>>>>>>> parent of e179a0c... Parameters as discussed
 	global RECENCY_MEMORY, FREQUENCY_MEMORY, FREQUENCY_INFLUENCE
 	jobs = plan['jobs']
 	steps = plan['steps']
@@ -168,7 +172,11 @@ def set_dynamic_parameters(plan, weights=(0.5, 2, 0.3)):
 	print("Weight of frequency of moves is %s" % FREQUENCY_INFLUENCE)
 	print("")
 
+<<<<<<< HEAD
 def tabu_search(instance, mode = MODE, weights=(0.5, 2, 0.3)):
+=======
+def tabu_search(instance, mode = MODE, weights=(10, 2, 0.25)):
+>>>>>>> parent of e179a0c... Parameters as discussed
 	instance_path = 'instances/' + instance + '.txt'
 	if not path.exists(instance_path):
 		raise SystemExit("There is no instance at %s" % instance_path)
